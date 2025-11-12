0fccb101f574838ee7c332ab67bd7ababc0a74a9
ValenMichou <lautaromichou@gmail.com>
2025-11-11 22:48:00 -0300

    alinear iconos pt1

--- HuellaSeguraX/js/comunidad.js
+++ HuellaSeguraX/js/comunidad.js
@@ -188,7 +188,7 @@ function toggleLike(boton) {
     .then(data => {
         if (data.success) {
             // Actualizar UI con el contador del servidor
-            boton.innerHTML = `❤️ ${data.total_likes}`;
+            boton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#EA3323"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Z"/></svg> ${data.total_likes}`;
             
             // Toggle de la clase liked
             if (estaLiked) {
